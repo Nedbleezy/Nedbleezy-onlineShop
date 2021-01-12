8d2da9989d914c4f98f0394875362f459d930f38
Nedbleezy <nedbleezytech@gmail.com>
2021-01-12 13:08:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <div className="grid-container">
+          <header>
+            <Header />
+          </header>
+          <main>products List</main>
+          <footer>All right is reserved</footer>
+        </div>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
